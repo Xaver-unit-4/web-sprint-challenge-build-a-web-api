947441d6ba3b05bc85726e3be6824106e26a7de9
Xaver Pinero <xaver.pinero@gmail.com>
2021-08-15 13:16:19 -0400

    added deps, set up server

--- api/server.js
+++ api/server.js
@@ -3,7 +3,24 @@ const server = express();
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
+//const actionRouter = require('./actions/actions-router');
 // Build your projects router in /api/projects/projects-router.js
+//const projectRouter = require('./projects/projects-router');
 // Do NOT `server.listen()` inside this file!
+
+server.use(express.json());
+//server.use('/api/actions', actionRouter);
+//server.use('/api/projects', projectRouter);
+
+server.get('/', (req,res) => {
+  res.status(200).send(`<h1> Let's write some code! YAY!<h1>`);
+});
+
+server.use(function (req,res) {
+  res.status(400).send(`We don't have time for this. Give a real page!`);
+});
+
+
+
 
 module.exports = server;
--- index.js
+++ index.js
@@ -12,3 +12,15 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+const express = require("express");
+
+const server = require('./api/server');
+
+const port = process.env.PORT || 5000;
+
+server.use(express.json());
+
+server.listen(port, () => {
+  console.log(`message in the terminal. Server started on localhost: ${port}`)
+});
+
