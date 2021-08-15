5e8335da4a069b013c68831bbb3ec9d9cf42acf8
Xaver Pinero <xaver.pinero@gmail.com>
2021-08-15 13:21:02 -0400

    project middleware

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,37 @@
 // add middlewares here related to projects
+const projectInfo = require('./projects-model');
+
+
+function logger(req, res, next) {
+  const time = new Date().toISOString()
+  console.log(`${time} ${req.ip} ${req.method} ${req.url}`);
+  next();
+}
+
+async function validateField(req, res, next) {
+  const { completed } = await req.body;
+  if (completed) {
+    next()
+  } else {
+    res.status(400).json({ message: "missing required completed status" })
+  }
+}
+
+async function validateProjectId(req, res, next) {
+  const project_id = req.params.id;
+  const project = await projectInfo.get(project_id)
+  if (project_id) {
+    req.project = project
+    next()
+  } else {
+    res.status(404).json({ message: "project does not exist" })
+  }
+}
+
+module.exports = {
+  logger,
+  validateField,
+  validateProjectId
+};
+
+
